bfa751cffa066b7762bd86e0abb0fccc220e3e1d
Yogue1997 <yousyogue@gmail.com>
2023-08-06 15:01:00 -0400

    npm i --save mongoose INSTALLED

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ app.use((req, res, next) => {
     next();
 })
 
+//mongodb+srv://yyogue:<password>@e-commerce.4dmlnlf.mongodb.net/
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 

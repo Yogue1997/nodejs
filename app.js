d85bb38e619c9985f7eb8dfca1c956c42e81418e
Yogue1997 <yousyogue@gmail.com>
2023-08-06 14:46:18 -0400

    Allow Access Control

--- app.js
+++ app.js
@@ -8,8 +8,18 @@ const productRoutes = require('./routes/products');
 const orderRoutes = require('./routes/order');
 
 app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-with, Content-Type, Accept, Authorization')
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+        return res.status(200).json({})
+    }
+    next();
+})
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);

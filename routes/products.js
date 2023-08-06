5045b7db764f1784dcd3d9bc3d8d609d95c85c58
Yogue1997 <yousyogue@gmail.com>
2023-08-06 16:25:26 -0400

    products set done

--- routes/products.js
+++ routes/products.js
@@ -4,9 +4,24 @@ const Product = require('./models/product')
 const mongoose = require('mongoose');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET requests to /products'
-    })
+    Product.find()
+        .exec()
+        .then(docs => {
+            console.log(docs)
+            // if(docs.length >= 0) {
+            res.status(200).json(docs)
+            // } else {
+            //     res.status(404).json({
+            //         message: 'No entries found'
+            //     })
+            // }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        })
 });
 
 router.post('/', (req, res, next) => {
@@ -16,49 +31,74 @@ router.post('/', (req, res, next) => {
         price: req.body.price,
     });
     product.save()
-    .then(result => {
-        console.log(result);
-        res.status(201).json({
-            message: 'Handling POST requests to /products',
-            createdProduct: result
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message: 'Handling POST requests to /products',
+                createdProduct: result
+            })
         })
-    })
-    .catch(err => {
-        console.log(err)
-        res.status(500).json({
-            error: err,
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                error: err,
+            })
         })
-    })
 });
 
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
     Product.findById(id)
-    .exec()
-    .then(doc => {
-        console.log("From database", doc)
-        if(doc){
-            res.status(200).json(doc)
-        } else {
-            res.status(404).json({message: 'No valid entry found for the provided ID'})
-        }
-    })
-    .catch(err => {
-        console.log(err)
-        res.status(500).json({error: err})
-    })
+        .exec()
+        .then(doc => {
+            console.log("From database", doc)
+            if (doc) {
+                res.status(200).json(doc)
+            } else {
+                res.status(404).json({ message: 'No valid entry found for the provided ID' })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: err })
+        })
 });
 
 router.patch('/:productId', (req, res, next) => {
-    res.status(200).json({
-        messahe: 'Updated product!'
-    })
+    const id = req.params.productId;
+    const updateOppertion = {}
+    for (const opperation of req.body) {
+        updateOppertion[opperation.propName] = opperation.value
+    }
+    Product.findOneAndUpdate({ _id: id }, { $set: updateOppertion })
+        .exec()
+        .then(result => {
+            console.log(result)
+            res.status(200).json(result)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                error: err
+            })
+        })
 });
 
 router.delete('/:productId', (req, res, next) => {
-    res.status(200).json({
-        messahe: 'Delated product!'
+    const id = req.params.productId;
+    Product.findByIdAndRemove({
+        _id: id,
     })
+        .exec()
+        .then(result => {
+            res.status(200).json(result)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err,
+            })
+        })
 });
 
 module.exports = router;

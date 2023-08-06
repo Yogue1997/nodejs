eee6fb75ce27b167932de3baf9b799a76d34e538
Yogue1997 <yousyogue@gmail.com>
2023-08-06 11:33:59 -0400

    GET and POST method working

--- app.js
+++ app.js
@@ -3,12 +3,7 @@ const app = express()
 
 const productRoutes = require('./routes/products')
 
-app.use('./', productRoutes)
+app.use('/products', productRoutes)
 
-// app.use((req, res, next) => {
-//     res.status(200).json({
-//         message: 'It works'
-//     })
-// })
 
 module.exports = app;
